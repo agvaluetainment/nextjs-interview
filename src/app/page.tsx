16f2ad5a2e0b3db69450ba463b989f58477d6470
Amado Guerrero <aguerrero@valuetainment.com>
2024-08-05 17:02:25 -0400

    added getDate button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,6 +61,12 @@ export default function Home() {
         >
           Reset
         </button>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+          onClick={() => onSubmit}
+        >
+          getDate
+        </button>
       </form>
     </main>
   );
